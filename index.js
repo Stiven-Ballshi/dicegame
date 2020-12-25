990935fd50ed9d58b542a4c28ec96cf1c791fe9e
Stiven-Ballshi <75454690+Stiven-Ballshi@users.noreply.github.com>
2020-12-25 01:29:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNum1 = Math.floor(Math.random()*6) + 1;
+document.querySelector(".img1").setAttribute("src","images/dice" + randomNum1 + ".png");
+var randomNum2 = Math.floor(Math.random()*6) + 1;
+document.querySelector(".img2").setAttribute("src","images/dice" + randomNum2 + ".png");
+
+
+if (randomNum1 > randomNum2){
+    document.querySelector("h1").innerHTML = "👍Player 1 Wins!";
+}
+else if (randomNum1 < randomNum2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!👍";
+}
+else {
+  document.querySelector("h1").innerHTML = "👍Draw!👍";
+}
